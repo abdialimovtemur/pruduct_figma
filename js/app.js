05c815da12abc484014c2251e68250f23f2a6752
abdialimovtemur <abdialimovtemur@gmail.com>
2024-05-31 23:40:24 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    header.classList.add("header__shrink");
+  } else {
+    header.classList.remove("header__shrink");
+  }
+});
